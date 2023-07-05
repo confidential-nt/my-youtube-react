2053ec15408aa3edfe377667edb952e9102d1ae7
confidential-nt <youyoy689@gmail.com>
2023-07-05 16:07:46 +0900

    [refactoring] 코드 좀 더 일관성 있게 바꾸기

--- src/components/VideoCardPlaceholder/VideoCardPlaceholder.jsx
+++ src/components/VideoCardPlaceholder/VideoCardPlaceholder.jsx
@@ -7,15 +7,14 @@ import {
 
 const VideoCardPlaceholder = (
   <div className="my-awesome-placeholder">
-    <RectShape
-      style={{ width: "100%", height: "7rem" }}
-      className="bg-slate-500"
-    />
+    <div className="w-full h-24">
+      <RectShape className=" bg-slate-500" />
+    </div>
     <div className="flex">
       <div className="w-6 h-6 mt-2 mr-1">
         <RoundShape className="bg-slate-500" />
       </div>
-      <TextRow className="block bg-slate-500" />
+      <TextRow className="bg-slate-500" />
     </div>
   </div>
 );
